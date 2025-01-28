2de21f165a534a01efa4d6eb70c37a1a7f7582c7
Kamikaze <ilhamsyawal87@gmail.com>
2025-01-28 16:43:17 +0700

    Mulai buat program yg masukin data dari node

---
+++ Roadmap Front-End/8.NodeJs/7.CoreModule/script.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const readLine = require("readLine");
+
+const rl = readLine.Interface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+//! 1. Ambil data dari jsonya pake readFile
+
+//! 2. Tambahin datanya dari inputan pake append data di array
+//? 2.1 Inputan pake question & simpen ke variable sementara
+
+//! 3. Timpa data dala pake data baru.
